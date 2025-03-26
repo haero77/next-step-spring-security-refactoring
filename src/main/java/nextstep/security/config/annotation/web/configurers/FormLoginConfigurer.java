39280c4a3d9f2b72ff2c0819c453e7d96efbc751
haero77 <shineecard5@gmail.com>
2025-03-26 21:27:03 +0900

    (feedback): Configurer의 init, configure 구분
    - init: 초기 설정, 준비 작업. 필터 설정에 필요한 의존성 주입
    - configure: init 단계에서 준비된 객체를 이용하여 필터를 설정하고, 필터 추가

--- src/main/java/nextstep/security/config/annotation/web/configurers/FormLoginConfigurer.java
+++ src/main/java/nextstep/security/config/annotation/web/configurers/FormLoginConfigurer.java
@@ -7,14 +7,15 @@ import nextstep.security.config.annotation.web.builders.HttpSecurity;
 
 public class FormLoginConfigurer implements SecurityConfigurer {
 
+    private AuthenticationManager authenticationManager;
+
     @Override
     public void init(HttpSecurity http) {
+        this.authenticationManager = http.getSharedObject(AuthenticationManager.class);
     }
 
     @Override
     public void configure(HttpSecurity http) {
-        AuthenticationManager manager = http.getSharedObject(AuthenticationManager.class);
-        UsernamePasswordAuthenticationFilter filter = new UsernamePasswordAuthenticationFilter(manager);
-        http.addFilter(filter);
+        http.addFilter(new UsernamePasswordAuthenticationFilter(this.authenticationManager));
     }
 }
